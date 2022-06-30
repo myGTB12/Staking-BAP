2ba85d79451afec6f784e4be288aa9c33c0ad95e
tungb <tungbo121@gmail.com>
2022-06-30 09:40:18 +0700

    rm

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,11 +1,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-// const provider = new ethers.providers.JsonRpcProvider("https://eth-mainnet.alchemyapi.io/v2/1ZxDq2dEuBsRzwD0GIjfMkAmqusvUUVK");
+// const provider = new ethers.providers.JsonRpcProvider("");
 
 describe("test1", function(){
   let provider;
-  provider = ethers.providers.JsonRpcProvider("https://eth-rinkeby.alchemyapi.io/v2/QyIByIEwYUjCiIzETl29fkdyXmEqanVR");
+  provider = ethers.providers.JsonRpcProvider("");
   
 
 })
