5c4d1f7a838fd07ee79896e4e1eb6bae4c8dd3b8
tungb <tungbo121@gmail.com>
2022-06-30 09:36:51 +0700

    rm

--- test/Staking-test.js
+++ test/Staking-test.js
@@ -2,6 +2,6 @@ const {ethers} = require("hardhat");
 const {expect} = require("chai");
 
 describe("Test staking contract", function(){
-    let provider = ethers.providers.JsonRpcProvider("https://eth-rinkeby.alchemyapi.io/v2/QyIByIEwYUjCiIzETl29fkdyXmEqanVR");
+    let provider = ethers.providers.JsonRpcProvider("");
     
 })
