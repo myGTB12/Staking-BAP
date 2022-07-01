840bcc2d846523964b627bf0217e36ce946fef5b
tungb <tungbo121@gmail.com>
2022-07-01 11:24:16 +0700

    add test

--- test/Staking-test.js
+++ test/Staking-test.js
@@ -1,7 +1,49 @@
+
 const {ethers} = require("hardhat");
 const {expect} = require("chai");
 
-describe("Test staking contract", function(){
-    let provider = ethers.providers.JsonRpcProvider("");
-    
+
+describe("Staking", function(){
+    let owner;
+    let address1;
+    let address2;
+    let Staking;
+    let stakingContract;
+    let rewardToken;
+    let RewardToken;
+    let LPToken;
+    let lptoken;
+
+    beforeEach(async function(){
+        Staking = await ethers.getContractFactory("Staking");
+        RewardToken = await ethers.getContractFactory("RewardTokenn");
+        [owner, address1, address2] = await ethers.getSigners();
+        stakingContract = await Staking.deploy(2000000, 1658880000);
+        rewardToken = await RewardToken.deploy();
+        LPToken = await ethers.getContractFactory("LPToken");
+        lptoken = await LPToken.deploy();
+        await lptoken.transfer(address1.address, 2000);
+    });
+
+    describe("Staking constract ", function(){
+        it("Constructor parameter should correct", async function(){
+            expect(await stakingContract.rewardPerBlock()).to.equal(2000000);    
+            expect(await stakingContract.startBlock()).to.equal(1658880000);    
+        });
+        
+        it("add", async function(){
+            await stakingContract.add(100, lptoken.address, rewardToken.address, false, 100);
+            expect(await stakingContract.poolLength()).to.equal(1); 
+             
+        });
+
+        it('deposit', async function(){
+            await stakingContract.add(100, lptoken.address, rewardToken.address, true, 100);
+            await lptoken.approve(stakingContract.address, 500);
+            await stakingContract.connect(address1).deposit(0, 500);
+            // await stakingContract.connect(address1).deposit(0, 0);
+            console.log(await lptoken.balanceOf(address1.address));
+            expect(await lptoken.balanceOf(address1.address)).to.equal(1950);
+        });
+    });
 })
